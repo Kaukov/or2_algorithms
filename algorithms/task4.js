9aa305d6f6937fdb72bd4ee3f409e336defa2660
Peter Kaukov <peter.kaukov@gmail.com>
2018-05-28 21:04:16 +0300

    Initial setup. Add basic console application setup

---
+++ algorithms/task4.js
@@ -0,0 +1,46 @@
+const _           = require('lodash')
+const CLI         = require('clui')
+const Spinner     = CLI.Spinner
+const chalk       = require('chalk')
+const inquirer    = require('../lib/inquirer')
+
+class task4 {
+  static solve ({ x0, a1, b1, a2, b2, kmax, epsx, epsf, c }) {
+    const con = c || 1
+    // x0 = (x10, x20)
+    // a1, b1, a2, b2
+    // c = const = 1
+    // kmax
+    // epsx
+    // epsf
+
+    // Rok = c / (k + 1)
+  }
+}
+
+function calculateGradient () {
+  // deltaf(xk) = (2(x1 - 1))
+  //              (2(x2 - 2))
+
+  // (n) =      n!
+  // ( ) = -------------
+  // (k) = k! * (n - 1)!
+}
+
+function calculateGradientNorm () {
+  //
+}
+
+function calculateGamma () {
+  //
+}
+
+function calculateRo (k, c) {
+  //
+}
+
+function calculateXj () {
+  //
+}
+
+module.exports.task3
---
+++ app.js
@@ -0,0 +1,54 @@
+const chalk       = require('chalk')
+const clear       = require('clear')
+const figlet      = require('figlet')
+
+const files = require('./lib/files')
+const inquirer  = require('./lib/inquirer')
+
+const task1 = null
+const task2 = null
+const task3 = null
+const task4 = require('./algorithms/task4')
+const task5 = null
+
+const tasks = [
+  task1,
+  task2,
+  task3,
+  task4,
+  task5
+]
+
+// process.argv
+// [2] = task
+
+// const showPassedParams = () => {
+//   const { x } = process.argv
+//   console.log(process.argv)
+//   console.log(x)
+// }
+//
+// showPassedParams()
+
+const run = async () => {
+  const credentials = await inquirer.askGithubCredentials()
+  const { username } = credentials
+  console.log(credentials)
+  console.log(credentials.username)
+  console.log(username)
+}
+
+clear()
+
+console.log(
+  chalk.yellow(
+    figlet.textSync('Ginit', { horizontalLayout: 'full' })
+  )
+)
+
+run()
+
+// if (files.directoryExists('.git')) {
+//   console.log(chalk.red('Already a git repository!'))
+//   process.exit()
+// }
